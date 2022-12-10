4e6f2037a0cb74b3a7d3e50f9cde20386caeba05
Yessenali Yerkebulan <yerkebulan.yessenali@narxoz.kz>
2022-12-10 20:48:24 +0600

    commit

--- skills.py
+++ skills.py
@@ -13,8 +13,21 @@ def browser():
 def offpc():
     os.system('shutdown')
 
+def game():
+    subprocess.Popen('C:/Program Files/Counter-Strike Source/Run_CSS.exe')
+
 def weather():
-    print('weather')
+	'''Для работы этого кода нужно зарегистрироваться на сайте
+	https://openweathermap.org или переделать на ваше усмотрение под что-то другое'''
+	try:
+		params = {'q': 'Almaty', 'units': 'metric', 'lang': 'ru', 'appid': '4db0bcb363208ddf403a9da11323059e'}
+		response = requests.get(f'https://api.openweathermap.org/data/2.5/weather', params=params)
+		if not response:
+			raise
+		w = response.json()
+		speaker(f"На улице {w['weather'][0]['description']} {round(w['main']['temp'])} градусов")
+	except:
+		speaker('Произошла ошибка при попытке запроса к ресурсу API, проверь код')
 
 def telegram():
     subprocess.Popen('C:/Users/Yerkebulan/AppData/Roaming/Telegram Desktop/Telegram.exe')
